0b2af996e8de667cc6988edb6d0309fd78d8e4d6
Francesco <francescocollotto@gmail.com>
2022-03-09 17:58:48 +0000

    add Players create method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,19 @@ class PlayersController < ApplicationController
   def index
     render json: Player.all
   end
+
+  def create
+    @player = Player.new(player_params)
+
+    if @player.save
+      render json: @player, status: :created
+    else
+      render json: @player.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def player_params
+      params.require(:player).permit(:name, :nationality, :birthday, :points)
+    end
 end
