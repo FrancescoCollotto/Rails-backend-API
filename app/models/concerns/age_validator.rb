9d184a0bc6f22db2e8676979729e241bfcc079ad
Francesco <francescocollotto@gmail.com>
2022-03-10 11:24:23 +0000

    add custom age_validator.rb to register players must be 16 years old at least

---
+++ app/models/concerns/age_validator.rb
@@ -0,0 +1,11 @@
+class AgeValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    p value
+    today = Date.today
+    min_age = today.prev_year(16)
+    age = Date.parse(value.to_s)
+    if age > min_age
+      record.errors.add(attribute, "Players must be at least 16 years old to enter the club")
+    end
+  end
+end
