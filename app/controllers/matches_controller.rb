1bbc75546645a78922ad352117b678493a3afe9f
Francesco <francescocollotto@gmail.com>
2022-03-10 17:42:32 +0000

    update Match controller create method to register a match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,26 @@
 class MatchesController < ApplicationController
+  POINTS_PERCENTAGE = 0.1
   def create
-    @match = Match.new(winner_id: 1, loser_id: 2)
-    if @match.save
-      render json: @match, status: :created
+    @winner = Player.find_by(name: params[:winner])
+    @loser = Player.find_by(name: params[:loser])
+    if @winner && @loser
+      @match = Match.new(winner_id: @winner.id, loser_id: @loser.id)
+      if @match.save
+        render json: @match, status: :created
+      else
+        render json: @match.errors, status: :unprocessable_entity
+      end
     else
-      render json: @match.errors, status: :unprocessable_entity
+      render json: { error: 'record not found' }, status: :unprocessable_entity
     end
   end
+
+  def update_players_points
+
+  end
+
+  private
+    def match_params
+      params.require(:match).permit(:winner_id, :loser_id)
+    end
 end
