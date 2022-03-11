6b8abdade0b6e0f89c274349ea1bf5663c58f3d6
Francesco <francescocollotto@gmail.com>
2022-03-11 16:36:11 +0000

    add filter logic and extra data to Players controller index method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,30 @@
 class PlayersController < ApplicationController
   def index
-    render json: Player.all
+    players = Player.all
+    
+    ranked_players = players.filter { |player| 
+      player_matches = Match.where(winner_id: player.id).or(Match.where(loser_id: player.id))
+      player_matches.count >= 3
+    }
+    .map { |player| {name: player.name, nationality: player.nationality, age: calculate_age(player.birthday), points: player.points, rank_name: rank_name(player.points)} }
+    .sort_by! { |player| player[:points] }.reverse!
+
+    unranked_players = players.filter { |player| 
+      player_matches = Match.where(winner_id: player.id).or(Match.where(loser_id: player.id))
+      player_matches.count < 3
+    }
+    .map { |player| {name: player.name, nationality: player.nationality, age: calculate_age(player.birthday), points: player.points, rank_name: "Unranked"} }
+    .sort_by! { |player| player[:points] }.reverse!
+   
+    all_players = ranked_players + unranked_players
+    
+    rank = 1
+    all_players.map!.with_index { |player, i| 
+      player[:rank] = rank
+      rank += 1 if (players[i + 1] && players[i + 1][:points] != player[:points]) || players[i + 1] && players[i + 1][:rank_name] != player[:rank_name]
+      player
+    }
+    
   end
 
   def create
@@ -16,5 +40,25 @@ class PlayersController < ApplicationController
   private
     def player_params
       params.require(:player).permit(:name, :nationality, :birthday, :points)
+    end
+
+    def rank_name(points)
+      if points < 3000
+        "Bronze"
+      elsif points < 5000
+        "Silver"
+      elsif points < 10000
+        "Gold"
+      else
+        "Supersonic Legend"
+      end
+    end
+
+    def calculate_age(dob)
+      dob = Date.parse(dob.to_s)
+      today = Date.today
+      age = today.year - dob.year
+      age -= 1 if today.prev_year(age) < dob
+      age
     end
 end
