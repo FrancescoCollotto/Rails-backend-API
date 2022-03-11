d71cf26ffbc696e1375f9ffd41f89e634a715a59
Francesco <francescocollotto@gmail.com>
2022-03-11 11:03:27 +0000

    add test for record not found in create Matches controller

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Matches", type: :request do
   describe "POST /matches" do
     before(:each) do
       @player1 = Player.create(name: "Roger Federer", nationality: "Switzerland", birthday: "08-08-1981")
-      @palyer2 = Player.create(name: "Rafael Nadal", nationality: "Spain", birthday: "03-06-1986")
+      @player2 = Player.create(name: "Rafael Nadal", nationality: "Spain", birthday: "03-06-1986")
     end
     # it "returns http success" do
     #   get "/matches/create"
@@ -15,6 +15,12 @@ RSpec.describe "Matches", type: :request do
       headers = { "CONTENT_TYPE" => "application/json" }
       post "/matches", :params => '{ "winner":"Roger Federer", "loser":"Rafael Nadal" }', :headers => headers
       expect(response).to have_http_status(:created)
+    end
+    
+    it "returns http unprocessable_entity status" do
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post "/matches", :params => '{ "winner":"Matteo Berrettini", "loser":"Rafael Nadal" }', :headers => headers
+      expect(response).to have_http_status(:unprocessable_entity)
     end 
   end
 
