292e68dcadb0ba5943b921d40c6e6bc4b954e638
Francesco <francescocollotto@gmail.com>
2022-03-11 11:23:09 +0000

    add test players points update after a match

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe "Matches", type: :request do
       @player1 = Player.create(name: "Roger Federer", nationality: "Switzerland", birthday: "08-08-1981")
       @player2 = Player.create(name: "Rafael Nadal", nationality: "Spain", birthday: "03-06-1986")
     end
-    # it "returns http success" do
-    #   get "/matches/create"
-    #   expect(response).to have_http_status(:success)
-    # end
 
     it "returns http created status" do
       headers = { "CONTENT_TYPE" => "application/json" }
@@ -21,7 +17,16 @@ RSpec.describe "Matches", type: :request do
       headers = { "CONTENT_TYPE" => "application/json" }
       post "/matches", :params => '{ "winner":"Matteo Berrettini", "loser":"Rafael Nadal" }', :headers => headers
       expect(response).to have_http_status(:unprocessable_entity)
-    end 
+    end
+    
+    it "change players points when registered in a match" do
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post "/matches", :params => '{ "winner":"Roger Federer", "loser":"Rafael Nadal" }', :headers => headers
+      winner = Player.find_by(name: "Roger Federer")
+      loser = Player.find_by(name: "Rafael Nadal")
+      expect(winner.points).to eq 1320
+      expect(loser.points).to eq 1080
+    end
   end
 
 end
